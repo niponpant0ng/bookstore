eb57d7ff5a9406facfa708c6686d66de2cd0caf9
nipon <nfl_falcon@hotmail.com>
2018-04-24 00:04:24 +0700

    Config checking resource security

---
+++ src/main/java/com/service/bookstore/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.service.bookstore.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * Created by nipon on 4/23/18.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .cors()
+                .and()
+            .csrf()
+                .disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/users")
+                    .permitAll()
+                .antMatchers(HttpMethod.GET, "/books")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated();
+    }
+}
